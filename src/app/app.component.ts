47fdfb7fa42092ca5a58bf7cd7e9337b9e884f43
sandeepsodhi <vickysodhi15@gmail.com>
2019-08-26 18:48:33 -0400

    angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app',
-  template : '<h1> my app</h1>' 
+  templateUrl : './app.component.html',
+  styleUrls: ['./app.component.css']
+
 })
 
-export class AppComponent{}
+export class AppComponent{
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  }
+}
